102e81308d58ed5fcda2771c328313712c8767ef
zanhua huang <yzanhua@gmail.com>
2022-05-01 15:55:51 -0500

    code: 1) rename funcs and vars, 2) return bufferSize + numCalls

--- share/bput_globals.F90
+++ share/bput_globals.F90
@@ -1,3 +1,7 @@
+  ! this module contains necessary methods and data sturctures that
+  ! implement pnet_cdf bput.
+  !
+  ! all methods's names begin with "Bput"
   module bput_globals
       implicit none
 
@@ -7,7 +11,7 @@
       integer :: allocated_
       contains
 
-      subroutine init_bput_globals(num_reqs)
+      subroutine BputInitGlobals(num_reqs)
           ! init reqs_ to have size num_reqs; possibly all -1?
           integer, intent(in) :: num_reqs
           
@@ -16,9 +20,9 @@
 
           num_reqs_ = num_reqs
           i_ = 1
-      end subroutine init_bput_globals
+      end subroutine BputInitGlobals
 
-      subroutine set_bput_globals_req(req_val)
+      subroutine BputSetNextReqVal(req_val)
           integer, intent(in) :: req_val
 
           if (i_ .LE. num_reqs_) then
@@ -28,15 +32,15 @@
               ! error?
           endif
           
-      end subroutine set_bput_globals_req
+      end subroutine BputSetNextReqVal
 
-      subroutine get_bput_globals_num_reqs(num_reqs)
+      subroutine BputGetNumOfReqs(num_reqs)
           integer, intent(out) :: num_reqs
 
           num_reqs = num_reqs_
-      end subroutine get_bput_globals_num_reqs
+      end subroutine BputGetNumOfReqs
 
-      subroutine get_bput_globals_reqs_at_i(req_idx, req_val)
+      subroutine BputGetReqVal(req_idx, req_val)
           integer, intent(in) :: req_idx
           integer, intent(out) :: req_val
 
@@ -46,9 +50,9 @@
               ! error?
               req_val = -1
           endif
-      end subroutine get_bput_globals_reqs_at_i
+      end subroutine BputGetReqVal
 
-      subroutine ZanhuaLowerCase(MemoryOrder,MemOrd)
+      subroutine BputLowerCase(MemoryOrder,MemOrd)
           character*(*) ,intent(in)  :: MemoryOrder
           character*(*) ,intent(out) :: MemOrd
           character*1                :: c
@@ -63,16 +67,16 @@
               if('A'<=c .and. c <='Z') MemOrd(i:i)=achar(iachar(c)+upper_to_lower)
           enddo
           return
-      end subroutine ZanhuaLowerCase
+      end subroutine BputLowerCase
 
-      SUBROUTINE ZanhuaGetDim(MemoryOrder,NDim,Status)
+      SUBROUTINE BputGetDim(MemoryOrder,NDim,Status)
           include 'wrf_status_codes.h'
           character*(*) ,intent(in)  :: MemoryOrder
           integer       ,intent(out) :: NDim
           integer       ,intent(out) :: Status
           character*3                :: MemOrd
         
-          call ZanhuaLowerCase(MemoryOrder,MemOrd)
+          call BputLowerCase(MemoryOrder,MemOrd)
           select case (MemOrd)
             case ('xyz','xzy','yxz','yzx','zxy','zyx','xsz','xez','ysz','yez')
               NDim = 3
@@ -89,35 +93,33 @@
           end select
           Status = WRF_NO_ERR
           return
-      END SUBROUTINE ZanhuaGetDim
+      END SUBROUTINE BputGetDim
 
-      SUBROUTINE ZanhuaGetSize(MemoryOrder, Size, ep1, ep2, ep3, sp1, sp2, sp3)
-          character*(*) ,intent(in)  :: MemoryOrder
-          integer, intent(out) :: Size
-          integer, intent(in) :: ep1, ep2, ep3, sp1, sp2, sp3
+      SUBROUTINE BputGetGridSize(p, sizeOut)
+          USE module_domain_type, ONLY : fieldlist
+          TYPE(fieldlist), intent(inout), POINTER :: p
+          integer, intent(out) :: sizeOut
           INTEGER :: ndim, ierr
-          ! CHARACTER*80 memord
-          ! memord = MemoryOrder
+
+          CALL BputGetDim(TRIM(p%MemoryOrder), ndim, ierr)
       
-          CALL ZanhuaGetDim(TRIM(MemoryOrder), ndim, ierr)
-      
-          Size = 0
+          sizeOut = 0
           IF (ndim .EQ. 0) THEN    
-            Size = 1
+            sizeOut = 1
           ELSE IF (ndim .EQ. 1) THEN
-            if (ep1 - sp1 > 0) &
-            Size = (ep1 - sp1 + 1)
+            if (p%ep1 - p%sp1 > 0) &
+            sizeOut = (p%ep1 - p%sp1 + 1)
           ELSE IF (ndim .EQ. 2) THEN
-            if (ep1 - sp1 > 0 .AND. ep2 - sp2 > 0) &
-            Size = (ep1 - sp1 + 1) * (ep2 - sp2 + 1) 
+            if (p%ep1 - p%sp1 > 0 .AND. p%ep2 - p%sp2 > 0) &
+            sizeOut = (p%ep1 - p%sp1 + 1) * (p%ep2 - p%sp2 + 1) 
           ELSE IF (ndim .EQ. 3) THEN
-            if (ep1 - sp1 > 0 .AND. ep2 - sp2 > 0 .AND. ep3 - sp3 > 0) &
-            Size = (ep1 - sp1 + 1) * (ep2 - sp2 + 1) * (ep3 - sp3 + 1)
+            if (p%ep1 - p%sp1 > 0 .AND. p%ep2 - p%sp2 > 0 .AND. p%ep3 - p%sp3 > 0) &
+            sizeOut = (p%ep1 - p%sp1 + 1) * (p%ep2 - p%sp2 + 1) * (p%ep3 - p%sp3 + 1)
           ENDIF
           return
-      END SUBROUTINE ZanhuaGetSize
+      END SUBROUTINE BputGetGridSize
 
-      SUBROUTINE ZanhuaGetSizeOfType(Type, Size)
+      SUBROUTINE BputGetSizeOfType(Type, Size)
           CHARACTER*1, intent(in) :: Type
           INTEGER, intent(out) :: Size
           ! LOGICAL :: temp_logical
@@ -136,91 +138,93 @@
             Size = SIZEOF(Size)
           ENDIF
           RETURN
-      END SUBROUTINE ZanhuaGetSizeOfType
+      END SUBROUTINE BputGetSizeOfType
 
-      subroutine BputGetBufferSize(p,switch,grid, zanhua_total_size)
+      subroutine BputGetBufferSizeAndNumCalls(fieldListPtr,switch,grid, totalSizeOut, numCallsOut)
           USE module_domain_type, ONLY : fieldlist
           USE module_domain
           USE module_io
-          TYPE(domain), intent(in) :: grid
-          TYPE( fieldlist ), intent(inout), POINTER :: p
+
+          ! input
+          TYPE(fieldlist), intent(in), POINTER :: fieldListPtr
           INTEGER, INTENT(IN) :: switch
-          integer, intent(out):: zanhua_total_size
-          integer :: zanhua_grid_size, zanhua_type_size
-          INTEGER newswitch, itrace
+          TYPE(domain), intent(in) :: grid
+
+          ! output
+          integer, intent(out):: totalSizeOut
+          integer, intent(out):: numCallsOut
+
+          integer :: gridSize, typeSize
+          INTEGER newSwitch, itrace
+          TYPE(fieldlist), POINTER :: p
+          p => fieldListPtr
           
-          newswitch = switch
-          zanhua_total_size = 0
+          ! init variables
+          newSwitch = switch
+          totalSizeOut = 0
+          numCallsOut = 0
+
           DO WHILE ( ASSOCIATED( p ) )
             IF ( p%ProcOrient .NE. 'X' .AND. p%ProcOrient .NE. 'Y' ) THEN
-              zanhua_grid_size = 0
+              gridSize = 0
               IF (p%Ndim .EQ. 0)  THEN
                 ! TODO: check 0 or 1
-                IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams,newswitch)) THEN
+                IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams,newSwitch)) THEN
                   IF ( in_use_for_config(grid%id,TRIM(p%VarName)) ) THEN
-                    zanhua_grid_size = 1
-                    ! zanhua_num_calls = zanhua_num_calls + 1
+                    ! set grid szie. equals 1 when p%Ndim == 0
+                    gridSize = 1
                     
-                    ! helper; print info
-                    zanhua_type_size = 4
-                    CALL ZanhuaGetSizeOfType(p%Type, zanhua_type_size)
+                    typeSize = 4
+                    CALL BputGetSizeOfType(p%Type, typeSize)
                     
-                    zanhua_total_size = zanhua_total_size + zanhua_grid_size * zanhua_type_size
-                    ! CALL ZanhuaGetSize(p%MemoryOrder, zanhua_grid_size, p%ep1, p%ep2, p%ep3, p%sp1, p%sp2, p%sp3)
+                    totalSizeOut = totalSizeOut + gridSize * typeSize
+                    numCallsOut = numCallsOut + 1
                   ENDIF
                 ENDIF
               ELSE IF (p%Ndim .EQ. 1) THEN
-                IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams,newswitch)) THEN
+                IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams,newSwitch)) THEN
                   IF ( in_use_for_config(grid%id,TRIM(p%VarName)) ) THEN
                     IF (switch.EQ.restart_only.OR.p%Ntl/100.EQ.mod(p%Ntl,100)) THEN
-                      ! zanhua_num_calls = zanhua_num_calls + 1
-                      CALL ZanhuaGetSize(p%MemoryOrder, zanhua_grid_size, p%ep1, p%ep2, p%ep3, p%sp1, p%sp2, p%sp3)
+                      CALL BputGetGridSize(p, gridSize)
 
-                      ! helper; print info
-                      zanhua_type_size = 4
-                      CALL ZanhuaGetSizeOfType(p%Type, zanhua_type_size)
-                      ! WRITE(wrf_err_message,*) "**** zanhua_size_of_each_call ", zanhua_grid_size * zanhua_type_size
-                      ! CALL wrf_debug( 0 , wrf_err_message )
-                      zanhua_total_size = zanhua_total_size + zanhua_grid_size * zanhua_type_size
+                      typeSize = 4
+                      CALL BputGetSizeOfType(p%Type, typeSize)
+                  
+                      totalSizeOut = totalSizeOut + gridSize * typeSize
+                      numCallsOut = numCallsOut + 1
                     ENDIF
                   ENDIF
                 ENDIF
               ELSE IF (p%Ndim .EQ. 2) THEN
-                IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams,newswitch)) THEN
+                IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams,newSwitch)) THEN
                   IF ( in_use_for_config(grid%id,TRIM(p%VarName)) .AND.  &
                     ( .NOT. p%subgrid_x .OR. (p%subgrid_x .AND. grid%sr_x .GT. 0) ) .AND. &
                     ( .NOT. p%subgrid_y .OR. (p%subgrid_y .AND. grid%sr_y .GT. 0) )       &
                   ) THEN
                     IF (switch.EQ.restart_only.OR.p%Ntl/100.EQ.mod(p%Ntl,100)) THEN
-                      ! zanhua_num_calls = zanhua_num_calls + 1
-                      CALL ZanhuaGetSize(p%MemoryOrder, zanhua_grid_size, p%ep1, p%ep2, p%ep3, p%sp1, p%sp2, p%sp3)
+                      CALL BputGetGridSize(p, gridSize)
 
-                      ! helper; print info
-                      zanhua_type_size = 4
-                      CALL ZanhuaGetSizeOfType(p%Type, zanhua_type_size)
-                      ! WRITE(wrf_err_message,*) "**** zanhua_size_of_each_call ", zanhua_grid_size * zanhua_type_size
-                      ! CALL wrf_debug( 0 , wrf_err_message )
-                      zanhua_total_size = zanhua_total_size + zanhua_grid_size * zanhua_type_size
+                      typeSize = 4
+                      CALL BputGetSizeOfType(p%Type, typeSize)
+                      totalSizeOut = totalSizeOut + gridSize * typeSize
+                      numCallsOut = numCallsOut + 1
 
                     ENDIF
                   ENDIF
                 ENDIF
               ELSE IF (p%Ndim .EQ. 3) THEN
-                IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams,newswitch)) THEN
+                IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams,newSwitch)) THEN
                   IF ( in_use_for_config(grid%id,TRIM(p%VarName)) .AND.  &
                     ( .NOT. p%subgrid_x .OR. (p%subgrid_x .AND. grid%sr_x .GT. 0) ) .AND. &
                     ( .NOT. p%subgrid_y .OR. (p%subgrid_y .AND. grid%sr_y .GT. 0) )       &
                   ) THEN
                     IF (switch.EQ.restart_only.OR.p%Ntl/100.EQ.mod(p%Ntl,100)) THEN
-                      ! zanhua_num_calls = zanhua_num_calls + 1
-                      CALL ZanhuaGetSize(p%MemoryOrder, zanhua_grid_size, p%ep1, p%ep2, p%ep3, p%sp1, p%sp2, p%sp3)
+                      CALL BputGetGridSize(p, gridSize)
 
-                      ! helper; print info
-                      zanhua_type_size = 4
-                      CALL ZanhuaGetSizeOfType(p%Type, zanhua_type_size)
-                      ! WRITE(wrf_err_message,*) "**** zanhua_size_of_each_call ", zanhua_grid_size * zanhua_type_size
-                      ! CALL wrf_debug( 0 , wrf_err_message )
-                      zanhua_total_size = zanhua_total_size + zanhua_grid_size * zanhua_type_size
+                      typeSize = 4
+                      CALL BputGetSizeOfType(p%Type, typeSize)
+                      totalSizeOut = totalSizeOut + gridSize * typeSize
+                      numCallsOut = numCallsOut + 1
                     ENDIF
                   ENDIF
                 ENDIF
@@ -228,16 +232,13 @@
                 IF (switch.EQ.restart_only.OR.p%Ntl/100.EQ.mod(p%Ntl,100)) THEN
                   DO itrace = PARAM_FIRST_SCALAR , p%num_table(grid%id)
                     
-                    IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams_table(grid%id,itrace)%stream,newswitch)) THEN
-                      ! zanhua_num_calls = zanhua_num_calls + 1
-                      CALL ZanhuaGetSize(p%MemoryOrder, zanhua_grid_size, p%ep1, p%ep2, p%ep3, p%sp1, p%sp2, p%sp3)
+                    IF ((p%Restart.AND.switch.EQ.restart_only).OR.on_stream(p%streams_table(grid%id,itrace)%stream,newSwitch)) THEN
+                      CALL BputGetGridSize(p, gridSize)
 
-                      ! helper; print info
-                      zanhua_type_size = 4
-                      CALL ZanhuaGetSizeOfType(p%Type, zanhua_type_size)
-                      ! WRITE(wrf_err_message,*) "**** zanhua_size_of_each_call ", zanhua_grid_size * zanhua_type_size
-                      ! CALL wrf_debug( 0 , wrf_err_message )
-                      zanhua_total_size = zanhua_total_size + zanhua_grid_size * zanhua_type_size
+                      typeSize = 4
+                      CALL BputGetSizeOfType(p%Type, typeSize)
+                      totalSizeOut = totalSizeOut + gridSize * typeSize
+                      numCallsOut = numCallsOut + 1
                     ENDIF
                   ENDDO
                   
@@ -248,6 +249,6 @@
             p => p%next
           ENDDO
         
-      end subroutine BputGetBufferSize
+      end subroutine BputGetBufferSizeAndNumCalls
 
   end module
